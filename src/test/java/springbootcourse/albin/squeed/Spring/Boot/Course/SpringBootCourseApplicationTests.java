2d66988de1dc87e8d99bbad634abc8261fb433cc
Albin Segestam <asegestam@Albins-MacBook-Pro.local>
2022-06-03 12:56:08 +0200

    Initial commit

---
+++ src/test/java/springbootcourse/albin/squeed/Spring/Boot/Course/SpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootcourse.albin.squeed.Spring.Boot.Course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
